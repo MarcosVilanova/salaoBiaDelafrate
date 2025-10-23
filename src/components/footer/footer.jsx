022c477d665de2425f411e89bd9aa2914ca957ab
marcos <marcosvilanova0000@gmail.com>
2025-10-23 13:43:05 -0300

    Correção de erros para deploy

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,59 +4,59 @@ import Logo from "../../assets/IconeeLogo/Logo.png";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <Container>
-        <Row className="py-5 align-items-center">
-          <Col xs={12} md={4} className=" mb-3 ">
-              <NavLink to="/">
-                  <img src={Logo} alt="Logo Bia Delefrate" className="logo" />
-              </NavLink>
-          </Col>
-          <Col xs={12} md={4} className="d-flex justify-content-center mb-3">
-            <ul className="links d-flex flex-column flex-md-row gap-md-4 ">
-              <li>
-                <a
-                  href="https://wa.me/5517988245969?text=Ol%C3%A1%2C%20quero%20agendar%20um%20hor%C3%A1rio"
-                  target="_blank"
-                >
-                  Agendar Agora
-                </a>
-              </li>
-              <li>
-                <NavLink to="/contato" className="footer-link">
-                  Localização
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/produtos" className="footer-link">
-                  Produtos
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/contato" className="footer-link">
-                  Contato
-                </NavLink>
-              </li>
-            </ul>
-          </Col>
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="py-5 align-items-center">
+                    <Col xs={12} md={4} className="mb-3">
+                        <NavLink to="/">
+                            <img src={Logo} alt="Logo Bia Delafrate" className="logo" />
+                        </NavLink>
+                    </Col>
+                    <Col xs={12} md={4} className="d-flex justify-content-center mb-3">
+                        <ul className="links d-flex flex-column flex-md-row gap-md-4">
+                            <li>
+                                <a
+                                    href="https://wa.me/5517988245969?text=Ol%C3%A1%2C%20quero%20agendar%20um%20hor%C3%A1rio"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Agendar Agora
+                                </a>
+                            </li>
+                            <li>
+                                <NavLink to="/contato" className="footer-link">
+                                    Localização
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/produtos" className="footer-link">
+                                    Produtos
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contato" className="footer-link">
+                                    Contato
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </Col>
 
-          <Col
-            xs={12}
-            md={4}
-            className="d-flex flex-column justify-content-center align-items-center align-items-md-end"
-          >
-              <p className="subtitulo mb-1">Projeto de Extensão - UniBarretos</p>
-              <p className="creditos mb-0">
-                  &copy; {new Date().getFullYear()} Bia Delafrate Liso Perfeito. Todos os direitos reservados.
-                  Desenvolvido por <a href="/Creditos" className="linkCreditos">Equipe Code</a>
-              </p>
-
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  );
+                    <Col
+                        xs={12}
+                        md={4}
+                        className="d-flex flex-column justify-content-center align-items-center align-items-md-end"
+                    >
+                        <p className="subtitulo mb-1">Projeto de Extensão - UniBarretos</p>
+                        <p className="creditos mb-0">
+                            &copy; {new Date().getFullYear()} Bia Delafrate Liso Perfeito. Todos os direitos reservados.
+                            Desenvolvido por <NavLink to="/Creditos" className="linkCreditos">Equipe Code</NavLink>
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
 };
 
 export default Footer;
